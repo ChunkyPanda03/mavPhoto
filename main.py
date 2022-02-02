ca059541248555f7d558b1957aa453adca48f949
ChunkyPanda03 <43187846+ChunkyPanda03@users.noreply.github.com>
2022-02-01 23:23:22 -0500

    Update main.py
    added documentation

--- main.py
+++ main.py
@@ -22,14 +22,18 @@ def capture(p_output):
     print (os.path.isdir(p_output))
     if os.path.isdir(p_output) != True:
         os.path.mkdir(p_output)
+    #create destination folder if folder does not exsist
     target = os.path.join(p_output, file_path.name)
     print('Copying image to', target)
     camera_file = camera.file_get(
         file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL)
+    #grab file from camera
     camera_file.save(target)
     d_date = datetime.date.today()
     n_time = time.strftime("%H:%M:%S")
     os.rename(target, "/tmp/camtest/" + d_date.strftime("%m.%d.%Y.") + n_time + ".jpg")
+    #this code will take a picture and then rename it to the date and time month.day.year.hour.minute.second
+    #also this code has an issue with repeated captures and high exposure times why is that?
 
 def main():
     output = "/tmp/camtest/"
